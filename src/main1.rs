0aee87258f16c3546f0b15331f03b74d9e78ba73
ZaneHisako <yassinetakiko@gmail.com>
2024-08-22 14:44:01 +0100

    working on adding multiple views

--- src/main1.rs
+++ src/main1.rs
@@ -1,5 +1,38 @@
+use iced::{
+    widget::{column, text},
+    Element,
+};
+
 #[derive(Debug)]
 pub enum App {
-    Variant1,
-    Variant2,
+    Main,
+    CharacterInfoPage,
+    RaceAndClassPage,
+}
+#[derive(Debug, Clone)]
+enum Message {
+    GoToMain,
+    GoToCharacterInfoPage,
+    GoToRaceAndClassPage,
+}
+
+impl App {
+    fn new() -> Self {
+        App::Main
+    }
+    fn view(&self) -> Element<Message> {
+        let content = match self {
+            App::Main => column![text("Main Page")].into(),
+            App::CharacterInfoPage => column![text("CharacterInfo Page")].into(),
+            App::RaceAndClassPage => column![text("RaceAndClass Page")].into(),
+        };
+        content
+    }
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::GoToMain => *self = App::Main,
+            Message::GoToCharacterInfoPage => *self = App::CharacterInfoPage,
+            Message::GoToRaceAndClassPage => *self = App::RaceAndClassPage,
+        };
+    }
 }
